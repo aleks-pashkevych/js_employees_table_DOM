4c9102a2f314dac7d89681c7a96fbb5379a28998
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-15 15:25:13 +0200

    implemented sorting the table in increasing and decreacint mode

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,76 @@
-'use strict';
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const heads = tHead.querySelectorAll('th');
+const rows = Array.from(tBody.querySelectorAll('tr'));
+let forward = true;
 
-// write code here
+const sortRowsForward = (column) =>
+  rows.sort((a, b) => {
+    const trimVal = (val) => {
+      let value = val
+        .querySelector(`td:nth-child(${column + 1})`)
+        .textContent.trim();
+
+      if (value[0] === '$') {
+        value = Number(value.replace(/[^0-9.-]+/g, ''));
+      }
+
+      return value;
+    };
+    const aTrimmed = trimVal(a);
+    const bTrimmed = trimVal(b);
+
+    if (typeof aTrimmed === 'string') {
+      return aTrimmed.localeCompare(bTrimmed);
+    }
+
+    return aTrimmed - bTrimmed;
+  });
+
+const sortRowsBackward = (column) =>
+  rows.sort((a, b) => {
+    const trimVal = (val) => {
+      let value = val
+        .querySelector(`td:nth-child(${column + 1})`)
+        .textContent.trim();
+
+      if (value[0] === '$') {
+        value = Number(value.replace(/[^0-9.-]+/g, ''));
+      }
+
+      return value;
+    };
+    const aTrimmed = trimVal(a);
+    const bTrimmed = trimVal(b);
+
+    if (typeof aTrimmed === 'string') {
+      return bTrimmed.localeCompare(aTrimmed);
+    }
+
+    return bTrimmed - aTrimmed;
+  });
+
+const rebuildTable = (column) => {
+  let sortedRows = '';
+
+  if (forward === true) {
+    sortedRows = sortRowsForward(column);
+  }
+
+  if (forward === false) {
+    sortedRows = sortRowsBackward(column);
+  }
+
+  forward = !forward;
+
+  while (tBody.firstChild) {
+    tBody.removeChild(tBody.firstChild);
+  }
+  tBody.append(...sortedRows);
+};
+
+for (const cell of heads) {
+  cell.addEventListener('click', (e) => {
+    rebuildTable(e.target.cellIndex);
+  });
+}
