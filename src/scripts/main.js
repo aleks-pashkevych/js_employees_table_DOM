1d738d259249d5561cb47cc5545f54a5bc898a6d
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-17 19:29:24 +0200

    sometest padded but need AI Body advices

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ const tBody = document.querySelector('tbody');
 const heads = tHead.querySelectorAll('th');
 const rows = Array.from(tBody.querySelectorAll('tr'));
 let forward = true;
+const form = document.createElement('form');
 
 const sortRowsForward = (column) =>
   rows.sort((a, b) => {
@@ -69,8 +70,80 @@ const rebuildTable = (column) => {
   tBody.append(...sortedRows);
 };
 
+const pushNotification = (titleText, description, typeText) => {
+  const popup = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const text = document.createElement('p');
+
+  popup.classList.add('notification', typeText);
+  popup.setAttribute('data-qa', 'notification');
+  h2.classList.add('title');
+  h2.textContent = titleText;
+  text.textContent = description;
+  popup.append(h2, text);
+
+  document.body.appendChild(popup);
+  setTimeout(() => popup.remove(), 4000);
+};
+
+const formDataValidator = (data) => {
+  if (data.name.length < 4) {
+    const type = 'error';
+
+    pushNotification('Error', 'Check Input Data', type);
+
+    return false;
+  }
+
+  return true;
+};
+
 for (const cell of heads) {
   cell.addEventListener('click', (e) => {
     rebuildTable(e.target.cellIndex);
   });
 }
+
+form.classList.add('new-employee-form');
+
+form.innerHTML = `
+  <label>Name: <input name="name" type="text" data-qa="name" required></label>
+  <label>Position: <input name="position" type="text" data-qa="position" required></label>
+  <label>Office: <select name="office" type="select" data-qa="office" required>
+  <option value="tokyo">Tokyo</option>
+  <option value="singapore">Singapore</option>
+  <option value="london">London</option>
+  <option value="new-york">New York</option>
+  <option value="edinburgh">Edinburgh</option>
+  <option value="san-francisco">San Francisco</option>
+  </select></label>
+  <label>Age: <input name="age" type="number" data-qa="age" required></label>
+  <label>Salary: <input name="salary" type="number" data-qa="salary" required></label>
+  <button type="submit">Save to table</button>
+  `;
+
+for (const row of rows) {
+  row.addEventListener('click', (e) => {
+    for (const el of rows) {
+      el.classList.remove('active');
+    }
+    row.classList.add('active');
+  });
+}
+
+document.body.append(form);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const employee = {
+    name: formData.get('name').trim(),
+    position: formData.get('position').trim(),
+    office: formData.get('office'),
+    age: Number(formData.get('age')),
+    salary: Number(formData.get('salary')),
+  };
+
+  formDataValidator(employee);
+});
